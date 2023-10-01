c6ebfa8d06b7daf9206dcace45d2958f010d520a
ShanksCodes <abhishektyagiofficial99@gmail.com>
2023-10-01 21:31:09 +0530

    very smol changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,204 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FirstFlutterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class FirstFlutterApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _FirstFlutterAppState createState() => _FirstFlutterAppState();
+}
+
+class _FirstFlutterAppState extends State<FirstFlutterApp> {
+  int incrementCount = 0;
+  int decrementCount = 0;
+  int squareCount = 0;
+  int primeCount = 0;
+  int evenOddCount = 0;
+  int palindromeCount = 0;
+  int factorialCount = 0;
+  int userInput = 0;
+
+  String resultText = '';
+
+  bool isPrime(int number) {
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    for (int i = 2; i <= sqrt(number); i++) {
+      if (number % i == 0) return false;
+    }
+    return true;
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  bool isEven(int number) {
+    return number % 2 == 0;
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  bool isPalindrome(int number) {
+    String numberStr = number.toString();
+    String reversedNumberStr = numberStr.split('').reversed.join('');
+    return numberStr == reversedNumberStr;
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  int calculateFactorial(int number) {
+    if (number == 0 || number == 1) return 1;
+    return number * calculateFactorial(number - 1);
+  }
 
-  final String title;
+  int findSquare(int number){
+    return number*number;
+  }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void updateResultText(String action, int value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      resultText =
+      "You have pushed the $action button this many times: $value\n";
+    });
+  }
+
+  void onButtonPress(String action) {
+    setState(() {
+      switch (action) {
+        case 'Increment':
+          incrementCount++;
+          userInput++;
+          updateResultText('increment', incrementCount);
+          resultText +='Incremented to $userInput\n';
+          break;
+        case 'Decrement':
+          decrementCount++;
+          userInput--;
+          updateResultText('decrement', decrementCount);
+          resultText +='Decremented to $userInput\n';
+          break;
+        case 'Square':
+          squareCount++;
+          updateResultText('square', squareCount);
+          resultText +='Square of $userInput is ${findSquare(userInput)}\n';
+          break;
+        case 'Prime':
+          primeCount++;
+          updateResultText('prime', primeCount);
+          resultText +=
+          '$userInput is ${isPrime(userInput) ? 'prime' : 'not prime'}\n';
+          break;
+        case 'Even/Odd':
+          evenOddCount++;
+          updateResultText('even/odd', evenOddCount);
+          resultText +=
+          '$userInput is ${isEven(userInput) ? 'even' : 'odd'}\n';
+          break;
+        case 'Palindrome':
+          palindromeCount++;
+          updateResultText('palindrome', palindromeCount);
+          resultText +=
+          '$userInput is ${isPalindrome(userInput) ? 'palindrome' : 'not palindrome'}\n';
+          break;
+        case 'Factorial':
+          factorialCount++;
+          updateResultText('factorial', factorialCount);
+          resultText +=
+          'Factorial of $userInput is ${calculateFactorial(userInput)}\n';
+          break;
+        case 'Reset':
+          incrementCount = 0;
+          decrementCount = 0;
+          squareCount = 0;
+          primeCount = 0;
+          evenOddCount = 0;
+          palindromeCount = 0;
+          factorialCount = 0;
+          resultText = '';
+          // Reset userInput to its original value
+          userInput =0;
+          break;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      title: 'FirstFlutterApp',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FirstFlutterApp'),
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Text(
+                'Enter a number:',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 200, // Set a specific width
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    userInput = int.tryParse(value) ?? 0;
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                resultText,
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton('Increment'),
+                  buildButton('Decrement'),
+                  buildButton('Square'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton('Prime'),
+                  buildButton('Even/Odd'),
+                  buildButton('Palindrome'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildButton('Factorial'),
+                  SizedBox(width: 10),
+                  buildButton('Reset'),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget buildButton(String action) {
+    return ElevatedButton(
+      onPressed: () {
+        onButtonPress(action);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        shape: MaterialStateProperty.all<OutlinedBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+      ),
+      child: Text(action),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutterpracs/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(FirstFlutterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
